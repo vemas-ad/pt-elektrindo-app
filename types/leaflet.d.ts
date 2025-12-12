f5153015784bcd59f15210b52c17be8b5bf563f0
Vemas Ardiyan Syah <vemasardiyan@student.ppns.ac.id>
2025-12-13 02:18:18 +0700

    chore: update config files for Next.js 16 compatibility

--- types/leaflet.d.ts
+++ types/leaflet.d.ts
@@ -1,7 +1,8 @@
-declare module "react-leaflet" {
+declare module "leaflet";
+
   export const MapContainer: any;
   export const TileLayer: any;
   export const Marker: any;
   export const Popup: any;
   export const useMap: any; // FIX ERROR useMap
-}
+
