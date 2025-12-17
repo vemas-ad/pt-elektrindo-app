380fe98b8e440737fd894feef19af2a4d90249fd
vemas_ad <vemassemail@example.com>
2025-12-17 21:17:41 +0700

    fix opennext build command for cloudflare pages

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,3 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
-import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+const config = {};
 
-export default defineCloudflareConfig({
-	incrementalCache: r2IncrementalCache,
-});
+export default config;
